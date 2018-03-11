c9f50558472e559dc983951a637e7c7c4e6544ec
RedHenDev <redsheepdev@gmail.com>
2018-03-11 20:14:56 +0000

    Seed tutorial code
    Gravity modifier set to 0 so we can fly.
    
    When user presses ‘p’, seed number iterated, so that terrain appears to
    ‘flow’.

---
+++ simpleVoxels_expanded_tuts/Assets/simpleVoxels/build.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class build : MonoBehaviour {
+
+	public Camera myCamera;
+
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			tryBuild ();
+		}
+		if (Input.GetMouseButtonUp (1)) {
+			tryDig ();
+		}
+	}
+
+
+	void tryDig(){
+		Ray ray = myCamera.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0f));
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 20.0f)) {
+			//Vector3 newBlockPos = hit.collider.transform.position;
+			//newBlockPos -= hit.normal * 1f;
+
+			if (hit.collider.gameObject.CompareTag ("synthetic")) {
+				DestroyObject (hit.collider.gameObject);
+			} else if (!hit.collider.gameObject.CompareTag ("synthetic")) {
+
+				hit.collider.transform.Translate (Vector3.down * 1f);
+			}
+
+		}
+	}
+
+	void tryBuild(){
+		Ray ray = myCamera.ScreenPointToRay(new Vector3(Screen.width/2, Screen.height/2, 0f));
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 20.0f)) {
+			Vector3 newBlockPos = hit.collider.transform.position;
+			newBlockPos += hit.normal * 1f;
+			GameObject 
+			newB = GameObject.Instantiate (hit.collider.gameObject);
+			newB.gameObject.tag = "synthetic";
+			newB.transform.position = newBlockPos;
+
+		}
+	}
+
+
+}
---
+++ simpleVoxels_expanded_tuts/Assets/simpleVoxels/simpleVoxelFarm.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simpleVoxelFarm : MonoBehaviour {
+
+
+	private GameObject currentBlockType;
+	public GameObject[] blockTypes;
+
+	[Tooltip("True for minecraft style voxels")]
+	public bool SnapToGrid = true;
+
+	public float amp = 10f;
+	public float freq = 10f;
+
+	public int seed = 0;
+
+	private Vector3 myPos;
+
+	void Start () {
+		generateTerrain ();
+	}
+	
+
+
+	void generateTerrain(){
+
+		myPos = this.transform.position;
+
+		int cols = 100;
+		int rows = 100;
+
+		for (int x = 0; x < cols; x++) {
+
+
+			for (int z = 0; z < rows; z++) {
+			
+				float y = Mathf.PerlinNoise (( seed + myPos.x + x) / freq, 
+					          (myPos.z + z) / freq) * amp;
+
+			
+				if (SnapToGrid) {
+					y = Mathf.Floor (y);
+				}
+
+				if (y > amp / 2)
+					currentBlockType = 
+					blockTypes [1];
+				else
+					currentBlockType = 
+					blockTypes [0];
+
+				GameObject newBlock = 
+					GameObject.Instantiate (currentBlockType);
+
+				newBlock.transform.position =
+					new Vector3 (myPos.x + x, y, myPos.z + z);
+
+				newBlock.transform.SetParent(this.transform);
+
+			}
+
+		}
+
+	
+	}
+
+	void reposTerrain(){
+
+		//myPos = this.transform.position;
+
+		int cols = 100;
+		int rows = 100;
+
+		for (int x = 0; x < cols; x++) {
+
+
+			for (int z = 0; z < rows; z++) {
+
+				float y = Mathf.PerlinNoise (( seed + myPos.x + x) / freq, 
+					(myPos.z + z) / freq) * amp;
+
+
+				if (SnapToGrid) {
+					y = Mathf.Floor (y);
+				}
+
+				int ind = ((z+1) * cols) + (x+1) - cols - 1;
+
+				//if (ind < this.transform.childCount) {
+					this.transform.GetChild (ind).transform.position =
+						new Vector3 (myPos.x + x, y, myPos.z + z);
+				//}
+				
+
+			}
+
+		}
+
+
+	}
+
+	void Update(){
+	
+		if (Input.GetKeyDown(KeyCode.P)) {
+			seed += 4;
+			reposTerrain();
+		}
+	
+	}
+
+
+}
