e0d1dd6e971778a32d1b0bd679c937ee5a5765fd
RedHenDev <redsheepdev@gmail.com>
2018-03-17 12:39:42 +0000

    Seed manager, and seed features

---
+++ simpleVoxels_expanded_tuts/Assets/simpleVoxels/seedManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seedManager : MonoBehaviour {
+
+	public bool randomize = true;
+	bool seedRandomized = false;
+
+	public int seed = 911;
+
+	void randomizeSeed(){
+		// We can only randomize the seed once.
+		if (seedRandomized)
+			return;
+		seed = Mathf.RoundToInt(Random.Range (0, 1000));
+		seedRandomized = true;
+	}
+
+	// Have we randomized our seed yet?
+	// If not, do this first before returning the seed number.
+	public int getSeed(){
+		if (randomize)
+			randomizeSeed ();
+		return seed;
+	}
+
+}
--- simpleVoxels_expanded_tuts/Assets/simpleVoxels/simpleVoxelFarm.cs
+++ simpleVoxels_expanded_tuts/Assets/simpleVoxels/simpleVoxelFarm.cs
@@ -14,20 +14,33 @@ public class simpleVoxelFarm : MonoBehaviour {
 	public float amp = 10f;
 	public float freq = 10f;
 
-	public int seed = 1;
+	public int seed = 0;
+	bool seeded = false;
+	private GameObject mySeeder;
 
 	private Vector3 myPos;
 
+	int currentX = 0;
+	int currentZ = 0;
+	bool finishedTerrain;
+	public bool useRunningTerrain = false;
+
 	void Start () {
-		generateTerrain ();
-		reposTerrain ();
+
+		mySeeder = GameObject.Find ("seed Manager");
+
+		seed = getSeed ();
+
+		myPos = this.transform.position;
+
+		if (useRunningTerrain==false){
+			generateTerrain ();
+		}
 	}
 	
 
 
 	void generateTerrain(){
-
-		myPos = this.transform.position;
 
 		int cols = 100;
 		int rows = 100;
@@ -40,6 +53,7 @@ public class simpleVoxelFarm : MonoBehaviour {
 				float y = Mathf.PerlinNoise (( seed + myPos.x + x) / freq, 
 					          (myPos.z + z) / freq) * amp;
 
+				//y += seedFeatures (x, z);
 			
 				if (SnapToGrid) {
 					y = Mathf.Floor (y);
@@ -69,7 +83,15 @@ public class simpleVoxelFarm : MonoBehaviour {
 	
 	}
 
+	float seedFeatures(int _x, int _z){
+
+		return Mathf.Sin (_x + _z);
+	}
+
 	void reposTerrain(){
+
+		if (useRunningTerrain)
+			return;
 
 		int cols = 100;
 		int rows = 100;
@@ -81,9 +103,7 @@ public class simpleVoxelFarm : MonoBehaviour {
 				float y = Mathf.PerlinNoise (( seed + myPos.x + x) / freq, 
 					(myPos.z + z) / freq) * amp;
 
-				// Added terrain features via seed.
-				if (seed % 9 == 0)
-					y += Mathf.Sin (z + x)/2.2f;
+				//y += seedFeatures (x, z);
 
 				if (SnapToGrid) {
 					y = Mathf.Floor (y);
@@ -103,15 +123,78 @@ public class simpleVoxelFarm : MonoBehaviour {
 
 
 	}
+
+	int getSeed(){
 		
+		return mySeeder.GetComponent<seedManager> ().getSeed ();
+
+	}
+
+	void runningTerrain(){
+		
+		if (finishedTerrain)
+			return;
+
+		int cols = 100;
+		int rows = 100;
+
+		currentX++;
+		if (currentX == cols) {
+			currentX = 1;
+			currentZ++;
+			if (currentZ == rows) {
+				finishedTerrain = true;
+			}
+		}
+
+		float y = Mathf.PerlinNoise (( seed + myPos.x + currentX) / freq, 
+			(myPos.z + currentZ) / freq) * amp;
+
+
+				if (SnapToGrid) {
+					y = Mathf.Floor (y);
+				}
+
+				if (y > amp / 2)
+					currentBlockType = 
+						blockTypes [1];
+				else
+					currentBlockType = 
+						blockTypes [0];
+
+				GameObject newBlock = 
+					GameObject.Instantiate (currentBlockType);
+
+				newBlock.transform.position =
+			new Vector3 (myPos.x + currentX, y, myPos.z + currentZ);
+
+				// Make the block a child, so that
+				// we can grab it later to reposition.
+				newBlock.transform.SetParent(this.transform);
+
+	}
 
 	void Update(){
 	
-		if (Input.GetKey(KeyCode.E)) {
+
+//		if (!seeded) {
+//			seed = getSeed ();
+//			seeded = true;
+//			//reposTerrain ();
+//		}
+
+		if (useRunningTerrain && seeded && Time.frameCount % 1 == 0) {
+			for (int i = 0; i < 64; i++) {
+				runningTerrain ();
+			}
+		}
+
+
+		if (Input.GetKeyUp(KeyCode.E)) {
 			seed += 1;
 			reposTerrain();
 		}
-		else if (Input.GetKeyDown(KeyCode.R)) {
+		else if (Input.GetKeyUp(KeyCode.R)) {
 			seed += 1000;
 			reposTerrain();
 		}
