42c3479684106516abc912eecea6cbc42be2fa14
RedHenDev <redsheepdev@gmail.com>
2018-05-07 21:02:30 +0100

    New tutorial -- mouse look 2

--- MouseLook_tut/MouseLook_tut/Assets/eyeLook.cs
+++ MouseLook_tut/MouseLook_tut/Assets/eyeLook.cs
@@ -5,32 +5,53 @@ using UnityEngine;
 public class eyeLook : MonoBehaviour {
 
 	// Mouse direction.
-	private Vector2 mD;
+	public Vector2 mD;
 
 	// The capsule parent!
-	private Transform myBody;
+	//private Transform myBody;
 
 	void Start () {
 
-		myBody = this.transform.parent.transform;
+		//myBody = this.transform.parent.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
 
 	}
 	
 
 	void Update () {
 
+
+		// Do we want to see mouse cursor again?
+		if (Input.GetKey (KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+
+		float movement = Input.GetAxis ("Vertical");
+		movement *= Time.deltaTime;
+
 		// How much has the mouse moved?
 		Vector2 mC = new Vector2
-			(Input.GetAxisRaw ("Mouse X"),
-				Input.GetAxisRaw("Mouse Y"));
+			(Input.GetAxisRaw ("Mouse X") * 3f,
+				Input.GetAxisRaw("Mouse Y") * 3f);
 
 		mD += mC;
 
+
+//			myBody.localRotation =
+//			Quaternion.AngleAxis (mD.x, Vector3.up);
+
+		Quaternion qR =
 		this.transform.localRotation =
+			Quaternion.AngleAxis (mD.x, Vector3.up);
+
+		// The actual rotation happening!
+		this.transform.localRotation = qR * 
 			Quaternion.AngleAxis (-mD.y, Vector3.right);
 
-		myBody.localRotation =
-			Quaternion.AngleAxis (mD.x, Vector3.up);
+		this.transform.Translate
+			(Vector3.forward * movement);
 
 	}
 }
--- MouseLook_tut/MouseLook_tut/Assets/walk.cs
+++ MouseLook_tut/MouseLook_tut/Assets/walk.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class walk : MonoBehaviour {
-
-	
+		
 
 	void Update () {
 
 		float movement = Input.GetAxis ("Vertical");
 		movement *= Time.deltaTime;
 
+		float sideStep = Input.GetAxis ("Horizontal");
+		sideStep *= Time.deltaTime;
+
+//		this.transform.Translate
+//		(Vector3.forward * movement);
+
+		if (this.GetComponent<Rigidbody> ().
+			velocity.magnitude < 5f) {
+			this.GetComponent<Rigidbody> ().AddForce
+		(this.transform.forward * movement * 1000f); 
+		}
+
 		this.transform.Translate
-		(Vector3.forward * movement);
-		
+		(Vector3.right * sideStep);
+	
+
 	}
 }
