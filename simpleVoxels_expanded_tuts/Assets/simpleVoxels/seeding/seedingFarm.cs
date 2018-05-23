91565ab6fce01eb4a9f5ef48addfb67ec5b6f4c5
RedHenDev <redsheepdev@gmail.com>
2018-05-23 22:00:58 +0100

    Biomes code added
    Need to correct final octave

--- simpleVoxels_expanded_tuts/Assets/simpleVoxels/seeding/seedingFarm.cs
+++ simpleVoxels_expanded_tuts/Assets/simpleVoxels/seeding/seedingFarm.cs
@@ -60,14 +60,28 @@ public class seedingFarm : MonoBehaviour {
 
 			for (int z = 0; z < rows; z++) {
 
-				float y = Mathf.PerlinNoise ((seed + myPos.x + x) / freq, 
+
+				bool makeTree = false;
+
+				float y = 0;
+
+				// First octave.
+				y = Mathf.PerlinNoise ((seed + myPos.x + x) / (freq*4f), 
+					(myPos.z + z) / (freq*4f)) * amp*4f;
+
+				// Orig octave.
+				y += Mathf.PerlinNoise ((seed + myPos.x + x) / freq, 
 					(myPos.z + z) / freq) * amp;
+
+				// Final octave.
+//				y += Mathf.PerlinNoise ((seed + myPos.x + x) / freq, 
+//					(myPos.z + z) / (freq*0.5f)) * (amp/0.5f);
 
 				if (SnapToGrid) {
 					y = Mathf.Floor (y);
 				}
 
-				if (y > amp / 2)
+				if (y > amp * 3f)
 					currentBlockType = 
 						blockTypes [1];
 				else
@@ -77,16 +91,61 @@ public class seedingFarm : MonoBehaviour {
 				GameObject newBlock = 
 					GameObject.Instantiate (currentBlockType);
 
+
+				// *&*&*&*&*&*&*&**&*&*&*&*&*&*&*&*&
+
+				// Biomes!
+
+				// Tree biome.
+//				if (myPos.z + z < 52f) {
+//					makeTree = true;
+//					y += Random.value * 1f;
+//				}
+
+				// Tundra biome.
+				float Bfreq = freq*6f;
+				if (myPos.z + z > 100f &&
+					(Mathf.PerlinNoise ((myPos.x + x) / Bfreq, 
+						(myPos.z + z) / Bfreq)
+						> 0.5f))
+					newBlock.GetComponent<Renderer> ().
+					material.color = Color.white;
+
+
+				// Taiga biome.
+				Bfreq = freq*2f;
+				if (Mathf.PerlinNoise ((myPos.x + x) / Bfreq, 
+					    (myPos.z + z) / Bfreq)
+					> 0.5f && myPos.z + z < 124f) {
+					newBlock.GetComponent<Renderer> ().material.color =
+						Color.blue;
+					makeTree = true;
+//						new Color (	0.9f + Random.value/10f, 
+//									0.9f+ Random.value/10f, 
+//									1f+ Random.value/10f);
+					//y += 2f;
+				}
+				// *&*&*&*&*&*&*&**&*&*&*&*&*&*&*&*&
+
+				// Finally, place block at correct position.
 				newBlock.transform.position =
 					new Vector3 (myPos.x + x * currentBlockType.transform.localScale.x, 
 						y, myPos.z + z * currentBlockType.transform.localScale.z);
 
+
+
+
+
+				//*********************************************************
+
 				// Now decide whether to create a tree!
-				if (Random.value * 100 < 0.1) {
+				if (Random.value * 100 < 1 && makeTree == true) {
 					float adjust = newBlock.transform.lossyScale.y / 2f;
 
 					GameObject treeBabe = 
 						GameObject.CreatePrimitive (PrimitiveType.Cube);
+
+
 
 					Vector3 tT = treeBabe.transform.localScale;
 					tT.y = Random.value * 24;
@@ -99,9 +158,13 @@ public class seedingFarm : MonoBehaviour {
 						new Vector3 (myPos.x + x * currentBlockType.transform.localScale.x, y + adjust, 
 							myPos.z + z * currentBlockType.transform.localScale.z);
 
+					treeBabe.GetComponent<Renderer> ().material.color = new Color(0.6f,0.1f,0.12f);
+
 					// Now for the head of the tree.
 					GameObject treeHead = 
 						GameObject.CreatePrimitive (PrimitiveType.Cube);
+
+					treeHead.GetComponent<Renderer> ().material.color = new Color (0f, 0.6f, 0f,0.7f);
 
 					treeHead.transform.localScale *= tT.y/4f + 4f;
 
